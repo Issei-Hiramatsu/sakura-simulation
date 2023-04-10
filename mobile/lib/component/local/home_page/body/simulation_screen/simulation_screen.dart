255a1f1ab5ae81b01fb88763e392114a2fbf3036
issei <issei.hiramatsu@gmail.com>
2023-04-10 14:23:36 +0900

    refactor: simulation_screen with hooks

--- mobile/lib/component/local/home_page/body/simulation_screen/simulation_screen.dart
+++ mobile/lib/component/local/home_page/body/simulation_screen/simulation_screen.dart
@@ -1,52 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class SimulationScreen extends StatefulWidget {
-  const SimulationScreen({
-    Key? key,
-  }) : super(key: key);
-  @override
-  _SimulationScreenState createState() => _SimulationScreenState();
-}
-
-class _SimulationScreenState extends State<SimulationScreen>
-    with TickerProviderStateMixin {
-  late AnimationController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 5));
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
+class SimulationScreen extends HookWidget {
+  const SimulationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller =
+        useAnimationController(duration: const Duration(seconds: 10));
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
+      children: [
         FadeTransition(
           opacity: Tween(begin: 1.0, end: 0.0).animate(
             controller.drive(CurveTween(curve: Curves.linear)),
           ),
+          //TODO: 写真予定場所
           child: const Placeholder(
               fallbackHeight: 300, fallbackWidth: double.infinity),
         ),
         FadeTransition(
           opacity: controller.drive(CurveTween(curve: Curves.linear)),
+          //TODO:  写真予定場所
           child: const Placeholder(
               fallbackHeight: 300, fallbackWidth: double.infinity),
         ),
-        FloatingActionButton(
-          onPressed: () {
-            controller.repeat();
-          },
-        ),
+        FloatingActionButton(onPressed: () {
+          controller.repeat();
+        })
       ],
     );
   }
