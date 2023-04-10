71db7c20b4594a8b88626b07dbbe348d60bd8d67
issei <issei.hiramatsu@gmail.com>
2023-04-10 16:27:40 +0900

    update: simulation_screen can be add function after animation ends

--- mobile/lib/component/local/home_page/body/simulation_screen/simulation_screen.dart
+++ mobile/lib/component/local/home_page/body/simulation_screen/simulation_screen.dart
@@ -8,21 +8,33 @@ class SimulationScreen extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    // AnimationControllerを作成
     final controller =
-        useAnimationController(duration: const Duration(seconds: 10));
-    controller.repeat();
+        useAnimationController(duration: const Duration(seconds: 4));
+
+// Animation<double>オブジェクトを作成
+    Animation<double> animation = Tween(begin: 1.0, end: 0.0).animate(
+      controller.drive(CurveTween(curve: Curves.linear)),
+    );
+
+// onEndプロパティに関数を設定
+    animation.addListener(() {
+      if (controller.status == AnimationStatus.completed) {
+        // アニメーションが終了したときに実行したい関数
+        controller.reset();
+        controller.forward();
+      }
+    });
+    controller.forward();
+
     return Stack(
       children: [
         FadeTransition(
-            opacity: Tween(begin: 1.0, end: 0.0).animate(
-              controller.drive(CurveTween(curve: Curves.linear)),
-            ),
+            opacity: animation,
             //TODO: 写真予定場所
             child: containerList[0]),
         FadeTransition(
-            opacity: Tween(begin: 0.0, end: 1.0).animate(
-              controller.drive(CurveTween(curve: Curves.linear)),
-            ),
+            opacity: animation,
             //TODO:  写真予定場所
             child: containerList[1]),
       ],
