ebe96688decf6e91b0514bfe7f42baac72b95056
issei <issei.hiramatsu@gmail.com>
2023-04-11 13:43:17 +0900

    refactor: integrat hooks& simulation_screen

--- mobile/lib/component/local/home_page/body/simulation_screen/simulation_screen.dart
+++ mobile/lib/component/local/home_page/body/simulation_screen/simulation_screen.dart
@@ -1,19 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'hooks/container_list.dart';
-import 'hooks/use_change_animated_object.dart';
-import 'hooks/use_fade_animation.dart';
 
-class SimulationScreen extends ConsumerWidget {
+class SimulationScreen extends HookConsumerWidget {
   const SimulationScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final beginIndex = ref.watch(beginIndexProvider);
-    final endIndex = ref.watch(endIndexProvider);
-    startFadeAnimation(ref);
+    //TODO: 複数の役割が持つように分離させたかったがhooksをうまく分離させることができなかった。
+    // アニメーションは動くが　数字を変えるとアニメーションが停止する
+    final beginIndex = useState(0);
+    final endIndex = useState(1);
+    void changeFadeInAnimatedObject() {
+      beginIndex.value = 1;
+      if (beginIndex.value == containerList.length) {
+        beginIndex.value = 0;
+      }
+    }
+
+    void changeFadeOutAnimatedObject() {
+      endIndex.value++;
+      if (endIndex.value == containerList.length) {
+        endIndex.value = 0;
+      }
+    }
+
+    final fadeInController =
+        useAnimationController(duration: const Duration(seconds: 10));
+    final fadeOutController =
+        useAnimationController(duration: const Duration(seconds: 10));
+
+    Animation<double> fadeInAnimation = Tween(begin: 1.0, end: 0.0).animate(
+      fadeInController.drive(CurveTween(curve: Curves.linear)),
+    );
+    Animation<double> fadeOutAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      fadeOutController.drive(CurveTween(curve: Curves.linear)),
+    );
+
+    void startFadeAnimation() {
+      fadeInController.forward();
+      fadeOutController.forward();
+      fadeInAnimation.addListener(() {
+        if (fadeInController.status == AnimationStatus.completed) {
+          changeFadeInAnimatedObject();
+          fadeInController.reset();
+          fadeInController.forward();
+        }
+      });
+
+      fadeOutAnimation.addListener(() {
+        if (fadeOutController.status == AnimationStatus.completed) {
+          changeFadeOutAnimatedObject();
+          fadeOutController.reset();
+          fadeOutController.forward();
+        }
+      });
+    }
+
+    startFadeAnimation();
 
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 12.sp, horizontal: 8.sp),
@@ -27,11 +74,11 @@ class SimulationScreen extends ConsumerWidget {
               //   fit: BoxFit.fill,
               //   height: double.infinity,
               // ),
-              child: containerList[beginIndex]),
+              child: containerList[beginIndex.value]),
           FadeTransition(
               opacity: fadeOutAnimation,
               //TODO:  写真予定場所
-              child: containerList[endIndex]),
+              child: containerList[endIndex.value]),
         ],
       ),
     );
