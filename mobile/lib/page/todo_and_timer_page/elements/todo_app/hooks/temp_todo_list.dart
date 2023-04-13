6f7a9d6a159bd7279e0f8e7b9785ba099396fd60
issei <issei.hiramatsu@gmail.com>
2023-04-13 14:53:17 +0900

    add: temp_todo_list

---
+++ mobile/lib/page/todo_and_timer_page/elements/todo_app/hooks/temp_todo_list.dart
@@ -0,0 +1,7 @@
+import 'package:sakura_simulation/domain/todo/todo.dart';
+
+final tempTodoList = [
+  const Todo(todo: 'イベントに参加する'),
+  const Todo(todo: 'duo3.0を進める'),
+  const Todo(todo: '数学の宿題を終わらせる'),
+];
