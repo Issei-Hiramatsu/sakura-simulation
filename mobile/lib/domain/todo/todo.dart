4de6a289327b5c4f0382ed469796ef34b998c823
issei <issei.hiramatsu@gmail.com>
2023-04-13 14:52:57 +0900

    add: domain/todo

---
+++ mobile/lib/domain/todo/todo.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'todo.freezed.dart';
+part 'todo.g.dart';
+
+@freezed
+class Todo with _$Todo {
+  const factory Todo({
+    @Default('') String todo,
+  }) = _Todo;
+
+  factory Todo.fromJson(Map<String, dynamic> json) => _$TodoFromJson(json);
+}
