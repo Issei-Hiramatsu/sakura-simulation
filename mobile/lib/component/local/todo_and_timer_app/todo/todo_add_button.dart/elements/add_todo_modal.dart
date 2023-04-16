166b8d42095bb857d9b11e9279f41090a8523dbd
issei <issei.hiramatsu@gmail.com>
2023-04-16 22:27:23 +0900

    add: bottom modal

--- mobile/lib/component/local/todo_and_timer_app/todo/todo_add_button.dart/elements/add_todo_modal.dart
+++ mobile/lib/component/local/todo_and_timer_app/todo/todo_add_button.dart/elements/add_todo_modal.dart
@@ -1,35 +1,33 @@
-// import 'package:flutter/material.dart';
-// import 'package:sakura_simulation/component/local/todo_and_timer_app/todo/todo_radio_button/todo_radio_button.dart';
-// import 'package:sakura_simulation/component/shared/token/color/color.dart';
-// import 'package:sakura_simulation/component/shared/token/space_box/space_box.dart';
+import 'package:flutter/material.dart';
+import 'package:sakura_simulation/component/local/todo_and_timer_app/todo/todo_radio_button/todo_radio_button.dart';
+import 'package:sakura_simulation/component/shared/token/color/color.dart';
+import 'package:sakura_simulation/component/shared/token/space_box/space_box.dart';
 
-//モーダル予定
-
-// Future showAddTodoModal(BuildContext context) {
-//   return showModalBottomSheet(
-//     context: context,
-//     builder: (BuildContext context) {
-//       return Container(
-//         height: 450,
-//         padding: const EdgeInsets.only(top: 15),
-//         margin: const EdgeInsets.only(top: 64),
-//         decoration: BoxDecoration(
-//           color: backgroundLightBlack,
-//           borderRadius: const BorderRadius.only(
-//             topLeft: Radius.circular(20),
-//             topRight: Radius.circular(20),
-//           ),
-//         ),
-//         child: Row(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: const [
-//             TodoRadioButton(isSelected: false),
-//             SpaceBox(width: 12),
-//             Expanded(child: TextField()),
-//           ],
-//         ),
-//       );
-//     },
-//     backgroundColor: transparent,
-//   );
-// }
+Future showAddTodoModal(BuildContext context) {
+  return showModalBottomSheet(
+    context: context,
+    builder: (BuildContext context) {
+      return Container(
+        height: 450,
+        padding: const EdgeInsets.only(top: 15),
+        margin: const EdgeInsets.only(top: 64),
+        decoration: BoxDecoration(
+          color: backgroundLightBlack,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            TodoRadioButton(isSelected: false),
+            SpaceBox(width: 12),
+            Expanded(child: TextField()),
+          ],
+        ),
+      );
+    },
+    backgroundColor: transparent,
+  );
+}
