cd19d699c1d9c51aaf9e891d9fb196a4f7f61a22
issei <issei.hiramatsu@gmail.com>
2023-04-17 11:49:16 +0900

    update: get keyboard height and change padding

--- mobile/lib/component/local/todo_and_timer_app/todo/todo_add_button.dart/elements/add_todo_modal.dart
+++ mobile/lib/component/local/todo_and_timer_app/todo/todo_add_button.dart/elements/add_todo_modal.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:sakura_simulation/component/local/todo_and_timer_app/todo/todo_radio_button/todo_radio_button.dart';
 import 'package:sakura_simulation/component/shared/token/color/color.dart';
-import 'package:sakura_simulation/component/shared/token/space_box/space_box.dart';
+
+import '../../../../../shared/token/text_style/text_style.dart';
 
 Future showAddTodoModal(BuildContext context) {
   return showModalBottomSheet(
     context: context,
     builder: (BuildContext context) {
-      return Container(
-        height: 450,
-        padding: const EdgeInsets.only(top: 15),
-        margin: const EdgeInsets.only(top: 64),
-        decoration: BoxDecoration(
-          color: backgroundLightBlack,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
+      final bottomSpace = MediaQuery.of(context).viewInsets.bottom;
+      return SingleChildScrollView(
+        reverse: true,
+        child: Padding(
+          padding: EdgeInsets.only(bottom: bottomSpace),
+          child: Container(
+            decoration: BoxDecoration(
+              color: backgroundLightBlack,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+                  child: TodoRadioButton(isSelected: false),
+                ),
+                Expanded(child: TextField()),
+              ],
+            ),
           ),
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            TodoRadioButton(isSelected: false),
-            SpaceBox(width: 12),
-            Expanded(child: TextField()),
-          ],
         ),
       );
     },
