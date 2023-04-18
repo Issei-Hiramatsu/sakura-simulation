3624abe9a7648af6b9012bdfb30248cffba28e13
issei <issei.hiramatsu@gmail.com>
2023-04-18 12:22:38 +0900

    add: timer_animation

---
+++ mobile/lib/component/local/todo_and_timer_app/timer/pomodoro_timer/elements/timer_progress_indicator/elements/timer_animation/timer_animation.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../../shared/token/color/color.dart';
+
+class TimerAnimation extends StatelessWidget {
+  const TimerAnimation({
+    super.key,
+    required this.strokeWidth,
+    required this.size,
+  });
+  final double size;
+  final double strokeWidth;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: size,
+      height: size,
+      child: TweenAnimationBuilder<double>(
+        tween: Tween<double>(begin: 0, end: 1),
+        duration: const Duration(milliseconds: 100000),
+        builder: (context, value, _) => CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation(gray),
+          value: value,
+          strokeWidth: strokeWidth,
+        ),
+      ),
+    );
+  }
+}
