611410319b414a6359d0678a07d0da5fa89b618c
issei <issei.hiramatsu@gmail.com>
2023-04-18 12:23:47 +0900

    add: timer_progress_indicator
    q
    
    wq

---
+++ mobile/lib/component/local/todo_and_timer_app/timer/pomodoro_timer/elements/timer_progress_indicator/timer_progress_indicator.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'elements/timer_animation.dart';
+import 'work_and_break_pie_chart/work_and_break_pie_chart.dart';
+
+class TimerProgressIndicator extends HookWidget {
+  const TimerProgressIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        WorkAndBreakPieChart(
+          size: 350.sp,
+          breakMinutes: 5,
+          workMinutes: 25,
+          chartWidth: 12.sp,
+        ),
+        TimerAnimation(
+          size: 350.sp,
+          strokeWidth: 12.sp,
+        ),
+      ],
+    );
+  }
+}
