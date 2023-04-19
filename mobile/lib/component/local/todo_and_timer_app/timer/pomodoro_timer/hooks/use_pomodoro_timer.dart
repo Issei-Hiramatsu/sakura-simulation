2a662b19383fdd06be5299808f2fac0f2b3b69db
issei <issei.hiramatsu@gmail.com>
2023-04-19 12:41:50 +0900

    update: stefull use_pomodoro

--- mobile/lib/component/local/todo_and_timer_app/timer/pomodoro_timer/hooks/use_pomodoro_timer.dart
+++ mobile/lib/component/local/todo_and_timer_app/timer/pomodoro_timer/hooks/use_pomodoro_timer.dart
@@ -1,46 +1,64 @@
 import 'dart:async';
 
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-final timerProvider = StateNotifierProvider((ref) => TimerController());
+class TimerSamplePage extends StatefulWidget {
+  const TimerSamplePage({super.key});
 
-class TimerController extends StateNotifier<DateTime> {
-  Timer? _timer;
-
-  TimerController() : super(DateTime.utc(0, 0));
-
-  void startTimer(DateTime seconds) {
-    state = seconds;
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      if (state == DateTime.utc(0, 0)) {
-        state.add(const Duration(seconds: 1));
-      } else {
-        timer.cancel();
-      }
-    });
-  }
-
-  void pauseTimer() {
-    _timer?.cancel();
-    _timer = null;
-  }
-
-  void resumeTimer() {
-    if (_timer == null && state == DateTime.utc(0, 0)) {
-      startTimer(state);
-    }
-  }
-
-  void resetTimer() {
-    _timer?.cancel();
-    _timer = null;
-    state = DateTime.utc(0, 0);
-  }
-
-//TODO:　必要かどうかチェック
+  // 状態を持ちたいので StatefulWidget を継承
   @override
-  void dispose() {
-    _timer?.cancel();
-    super.dispose();
+  _TimerSamplePageState createState() => _TimerSamplePageState();
+}
+
+class _TimerSamplePageState extends State<TimerSamplePage> {
+  late Timer _timer; // この辺が状態
+  late DateTime _time;
+
+  @override
+  void initState() {
+    // 初期化処理
+    _time = DateTime.utc(0, 0, 0);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // setState() の度に実行される
+    return Scaffold(
+        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text(
+        DateFormat.Hms().format(_time),
+        style: Theme.of(context).textTheme.headline2,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              // Stopボタンタップ時の処理
+
+              if (_timer != null && _timer.isActive) _timer.cancel();
+            },
+            child: const Text("Stop"),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              // Startボタンタップ時の処理
+              _timer = Timer.periodic(
+                const Duration(seconds: 1), // 1秒毎に定期実行
+                (Timer timer) {
+                  setState(() {
+                    // 変更を画面に反映するため、setState()している
+                    _time = _time.add(const Duration(seconds: 1));
+                  });
+                },
+              );
+            },
+            child: const Text("Start"),
+          ),
+        ],
+      )
+    ]));
   }
 }
