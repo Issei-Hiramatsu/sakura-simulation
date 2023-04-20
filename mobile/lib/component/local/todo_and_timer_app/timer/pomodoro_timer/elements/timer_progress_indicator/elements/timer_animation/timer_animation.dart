ae4dcdfeeb55de42b873ff238855903e8e455ce4
issei <issei.hiramatsu@gmail.com>
2023-04-20 22:40:29 +0900

    update: timerAnimation started anytime

--- mobile/lib/component/local/todo_and_timer_app/timer/pomodoro_timer/elements/timer_progress_indicator/elements/timer_animation/timer_animation.dart
+++ mobile/lib/component/local/todo_and_timer_app/timer/pomodoro_timer/elements/timer_progress_indicator/elements/timer_animation/timer_animation.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:sakura_simulation/component/local/todo_and_timer_app/timer/pomodoro_timer/hooks/temp_user_settings.dart';
 
 import '../../../../../../../../shared/token/color/color.dart';
-import '../../../../hooks/temp_user_settings.dart';
 
 class TimerAnimation extends StatelessWidget {
   const TimerAnimation({
     super.key,
     required this.strokeWidth,
     required this.size,
+    required this.animationStatusEnd,
   });
   final double size;
   final double strokeWidth;
+  final double animationStatusEnd;
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +20,7 @@ class TimerAnimation extends StatelessWidget {
       width: size,
       height: size,
       child: TweenAnimationBuilder<double>(
-        tween: Tween<double>(begin: 0, end: 1),
+        tween: Tween<double>(begin: 0, end: animationStatusEnd),
         duration: const Duration(minutes: workTime),
         builder: (context, value, _) => CircularProgressIndicator(
           valueColor: AlwaysStoppedAnimation(gray),
--- mobile/lib/component/local/todo_and_timer_app/timer/pomodoro_timer/elements/timer_progress_indicator/timer_progress_indicator.dart
+++ mobile/lib/component/local/todo_and_timer_app/timer/pomodoro_timer/elements/timer_progress_indicator/timer_progress_indicator.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../hooks/temp_user_settings.dart';
+import '../../pomodoro_timer.dart';
 import 'elements/timer_animation/timer_animation.dart';
 import 'elements/work_and_break_pie_chart/work_and_break_pie_chart.dart';
 
@@ -10,6 +12,8 @@ class TimerProgressIndicator extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final timer = ref.watch(cooperationTimerProvider) / 60 / 100;
+    final end = timer / workTime;
     return Stack(
       children: [
         WorkAndBreakPieChart(
@@ -21,6 +25,7 @@ class TimerProgressIndicator extends HookConsumerWidget {
         TimerAnimation(
           size: 350.sp,
           strokeWidth: 12.sp,
+          animationStatusEnd: end,
         ),
       ],
     );
