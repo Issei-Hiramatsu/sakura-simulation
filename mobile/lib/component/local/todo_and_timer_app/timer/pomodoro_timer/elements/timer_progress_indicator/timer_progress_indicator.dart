8a59f9d07ab0fbafa58844e7e03721cb46c57f5d
issei <issei.hiramatsu@gmail.com>
2023-04-24 15:19:27 +0900

    update: Animation to be linked to timer

--- mobile/lib/component/local/todo_and_timer_app/timer/pomodoro_timer/elements/timer_progress_indicator/timer_progress_indicator.dart
+++ mobile/lib/component/local/todo_and_timer_app/timer/pomodoro_timer/elements/timer_progress_indicator/timer_progress_indicator.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../pomodoro_timer.dart';
 import 'elements/timer_animation/timer_animation.dart';
 import 'elements/work_and_break_pie_chart/work_and_break_pie_chart.dart';
+import 'hooks/use_pomodoro_timer.dart';
 
 class TimerProgressIndicator extends HookConsumerWidget {
   const TimerProgressIndicator({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final timer = ref.watch(cooperationTimerProvider);
+    final timerRatio = ref.watch(timerAnimationParameterProvider);
+
     return Stack(
       children: [
         WorkAndBreakPieChart(
@@ -23,7 +24,7 @@ class TimerProgressIndicator extends HookConsumerWidget {
         TimerAnimation(
           size: 350.sp,
           strokeWidth: 12.sp,
-          animationStatusEnd: 1,
+          animationStatusEnd: timerRatio,
         ),
       ],
     );
