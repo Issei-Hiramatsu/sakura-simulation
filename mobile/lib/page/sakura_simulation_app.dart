21d04b804d800b70d7f5df3484dfa0bb00f4408b
issei <issei.hiramatsu@gmail.com>
2023-05-15 15:55:25 +0900

    refactor: add Calendar to main

--- mobile/lib/page/sakura_simulation_app.dart
+++ mobile/lib/page/sakura_simulation_app.dart
@@ -4,6 +4,8 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import '/page/home/home_page.dart';
 import '/page/todo_and_timer_page/todo_and_timer_page.dart';
 import '../component/shared/single/bottom_navigation/bottom_navigation.dart';
+import 'calendar/calendar.dart';
+import 'profile/profile_page.dart';
 
 class SakuraSimulationApp extends HookWidget {
   const SakuraSimulationApp({Key? key}) : super(key: key);
@@ -13,8 +15,11 @@ class SakuraSimulationApp extends HookWidget {
     List pageList = [
       const HomePage(),
       const TodoAndTimerPage(),
-      Container(),
-      Container(),
+      TableEventsExample(),
+      const SlackProfilePage(
+        name: 'Issei',
+        photoUrl: '0',
+      ),
     ];
     final selectedIndex = useState(0);
     return SafeArea(
