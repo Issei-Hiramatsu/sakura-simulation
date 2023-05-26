171be30b359301eddd5357d71b9b4c1565d3af63
issei <issei.hiramatsu@gmail.com>
2023-05-26 15:21:07 +0900

    add&update: default_user_image(free license) , default_data

--- mobile/lib/domain/default_data.dart
+++ mobile/lib/domain/default_data.dart
@@ -1,11 +1,11 @@
 import 'package:sakura_simulation/domain/user/user.dart';
 
-const defaultData = [
+final defaultData = [
   User(
-    id: 1,
-    email: '',
-    userName: '',
-    userImagePath: '',
-    firstTimeUsing: '',
+    id: 1, //アプリ内ユーザ識別
+    email: 'issei.hiramatsu@gmail.com', //ユーザ識別よう
+    userName: 'issei',
+    userImagePath: '/assets/images/kkrn_user_icons/kkrn_icon_user_1.png',
+    firstTimeUsing: DateTime.utc(2023, 4, 1),
   )
 ];
