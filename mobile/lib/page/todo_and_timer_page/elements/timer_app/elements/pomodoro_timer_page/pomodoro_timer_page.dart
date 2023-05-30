1325a831a5d22c7351ba2bf9e5db71d5b0f631dc
issei <issei.hiramatsu@gmail.com>
2023-05-30 13:06:08 +0900

    refactor: pomodoroTimerAppPage Appbar change to SharedAppBar

--- mobile/lib/page/todo_and_timer_page/elements/timer_app/elements/pomodoro_timer_page/pomodoro_timer_page.dart
+++ mobile/lib/page/todo_and_timer_page/elements/timer_app/elements/pomodoro_timer_page/pomodoro_timer_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sakura_simulation/component/shared/single/shared_app_bar/shared_app_bar.dart';
 import 'package:sakura_simulation/component/shared/token/space_box/space_box.dart';
 
 import '../../../../../../component/local/todo_and_timer_app/timer/timer_card/elements/pomodoro_timer/pomodoro_timer.dart';
 import '../../../../../../component/local/todo_and_timer_app/todo/hooks/temp_todo_list.dart';
 import '../../../../../../component/local/todo_and_timer_app/todo/todo_list/todo_list.dart';
+import '../../../../../../component/shared/token/color/color.dart';
+import '../../../../../../component/shared/token/text_style/text_style.dart';
 import '../../../../../../domain/user/user.dart';
 
 class PomodoroTimerPage extends ConsumerWidget {
@@ -19,7 +22,13 @@ class PomodoroTimerPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final todoList = ref.watch(tempTodoListProvider);
     return Scaffold(
-      appBar: AppBar(),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(46.sp),
+        child: SharedAppBar(
+          titleText: 'タイマー',
+          textStyle: titleMedium(white),
+        ),
+      ),
       body: Column(
         children: [
           PomodoroTimer(
