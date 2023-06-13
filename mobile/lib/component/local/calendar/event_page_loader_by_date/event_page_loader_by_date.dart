ff573c09db3149e0ea065e5ccc9931523078b928
issei <issei.hiramatsu@gmail.com>
2023-06-13 11:07:48 +0900

    refactor: event_page_loader_by_date

--- mobile/lib/component/local/calendar/event_page_loader_by_date/event_page_loader_by_date.dart
+++ mobile/lib/component/local/calendar/event_page_loader_by_date/event_page_loader_by_date.dart
@@ -7,19 +7,23 @@ import 'package:sakura_simulation/importer.dart';
 import '../../todo_and_timer_app/todo/todo_list/elements/hooks/fetch_todo_list.dart';
 import 'elements/event_list_and_graph/event_list_and_graph.dart';
 
-class EventListPageView extends HookConsumerWidget {
-  const EventListPageView({
+class EventPageLoaderByDate extends HookConsumerWidget {
+  const EventPageLoaderByDate({
     super.key,
     required this.onPageChanged,
     required this.goBackPage,
     required this.goFrontPage,
-    required this.focusedDay,
+    required this.focusedDate,
+    required this.firstPageDate,
+    required this.lastPageDate,
   });
 
   final VoidCallback onPageChanged;
   final VoidCallback goBackPage;
   final VoidCallback goFrontPage;
-  final DateTime focusedDay;
+  final DateTime focusedDate;
+  final DateTime firstPageDate;
+  final DateTime lastPageDate;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -35,16 +39,16 @@ class EventListPageView extends HookConsumerWidget {
         } else if (index >= currentIndex.value) {
           goFrontPage();
         }
-        onPageChanged();
         currentIndex.value = index;
+        onPageChanged();
       },
+      //FIXME: データの処理構造が違うかも
       itemCount: 100, //100で仮置きする。ただし増えすぎても負荷の原因となる気がするので検証してからこの値を変更してほしい。
       itemBuilder: (context, index) {
-        final focusedDate =
-            DateTime.utc(focusedDay.year, focusedDay.month, focusedDay.day);
         return ref.watch(fetchAllTodoList(context)).when(
-              data: (dataList) =>
-                  EventListView(eventList: dataList[focusedDate] ?? []),
+              data: (dataList) => EventListView(
+                eventList: dataList[focusedDate] ?? [],
+              ),
               error: (error, _) => const Icon(Icons.error),
               loading: () => const Center(
                 child: CircularProgressIndicator(
@@ -56,3 +60,6 @@ class EventListPageView extends HookConsumerWidget {
     );
   }
 }
+
+
+  // if (focusedDate == firstPageDate || focusedDay == lastPageDate) {}
