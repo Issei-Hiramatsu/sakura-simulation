1acfef4d40a7d46b54c8ecc2d965f07d759450fb
issei <issei.hiramatsu@gmail.com>
2023-06-24 17:37:06 +0900

    fix: return new value when update testTodoList

--- mobile/lib/infrastructure/test_data/test_todo_list_repository.dart
+++ mobile/lib/infrastructure/test_data/test_todo_list_repository.dart
@@ -1,50 +1,50 @@
 import '../../domain/user/elements/todo/todo.dart';
 
 //テスト用のRepository Firebaseの接続を必要としない
-class TestTodoListRepository extends ITodoListRepository {
-  final testTodoList = {
-    DateTime(2023, 4, 1): [
-      const Todo(
-        id: 1,
-        title: 'イベントに参加する',
-        isCompleted: false,
-        isFavorite: false,
-      ),
-      const Todo(
-        id: 2,
-        title: 'duo3.0を進める',
-        isCompleted: false,
-        isFavorite: false,
-      ),
-      const Todo(
-        id: 3,
-        title: '数学の宿題を終わらせる',
-        isCompleted: false,
-        isFavorite: false,
-      ),
-    ],
-    DateTime(2023, 4, 3): [
-      const Todo(
-        id: 4,
-        title: 'イベントに参加しない',
-        isCompleted: false,
-        isFavorite: false,
-      ),
-      const Todo(
-        id: 5,
-        title: '英語',
-        isCompleted: false,
-        isFavorite: false,
-      ),
-      const Todo(
-        id: 6,
-        title: 'タスク管理',
-        isCompleted: false,
-        isFavorite: false,
-      ),
-    ],
-  };
+final testTodoList = {
+  DateTime(2023, 4, 1): [
+    const Todo(
+      id: 1,
+      title: 'イベントに参加する',
+      isCompleted: false,
+      isFavorite: false,
+    ),
+    const Todo(
+      id: 2,
+      title: 'duo3.0を進める',
+      isCompleted: false,
+      isFavorite: false,
+    ),
+    const Todo(
+      id: 3,
+      title: '数学の宿題を終わらせる',
+      isCompleted: false,
+      isFavorite: false,
+    ),
+  ],
+  DateTime(2023, 4, 3): [
+    const Todo(
+      id: 4,
+      title: 'イベントに参加しない',
+      isCompleted: false,
+      isFavorite: false,
+    ),
+    const Todo(
+      id: 5,
+      title: '英語',
+      isCompleted: false,
+      isFavorite: false,
+    ),
+    const Todo(
+      id: 6,
+      title: 'タスク管理',
+      isCompleted: false,
+      isFavorite: false,
+    ),
+  ],
+};
 
+class TestTodoListRepository extends ITodoListRepository {
   @override
   Stream<Map<DateTime, List<Todo>>> fetchAllTodoList() {
     return Stream.value(testTodoList);
@@ -59,8 +59,8 @@ class TestTodoListRepository extends ITodoListRepository {
   Stream<List<Todo>> fetchLatestTodoList() {
     final sortedList = testTodoList.entries.toList()
       ..sort((a, b) => a.key.compareTo(b.key));
-    final newestKey = sortedList.last;
-    return Stream.value(testTodoList[newestKey] ?? []);
+    final latestList = sortedList.last;
+    return Stream.value(testTodoList[latestList.key] ?? []);
   }
 
   @override
