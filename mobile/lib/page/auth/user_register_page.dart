aa008d8a47a4d96496925e6097e95232c9cc2800
issei <issei.hiramatsu@gmail.com>
2023-08-05 14:16:10 +0900

    add: commponent sharedTextField

--- mobile/lib/page/auth/user_register_page.dart
+++ mobile/lib/page/auth/user_register_page.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:uuid/uuid.dart';
-import '/importer.dart';
+import '../../importer.dart';
 
-import '/component/shared/token/navigator/navigator.dart';
-import '/infrastructure/user_repository.dart';
 import '/page/sakura_simulation_app.dart';
-import '/domain/user/elements/account_level/account_level.dart';
-import '../../domain/user/elements/timer_log/timer_log.dart';
-import '/domain/user/user.dart';
-import '/use_case/user_use_case.dart';
+import '../../component/local/auth/hooks/handle_firebase_auth_error.dart';
+import '../../component/shared/single/shared_app_bar/shared_app_bar.dart';
+import '../../component/shared/single/shared_text_field/shared_text_field.dart';
+import '../../component/shared/token/navigator/navigator.dart';
 
-import '/component/shared/single/shared_app_bar/shared_app_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-final registerUserProvider =
-    Provider((ref) => UserUseCase(userRepository: UserRepository()));
+// final registerUserProvider =
+//     Provider((ref) => UserUseCase(userRepository: UserRepository()));
 
 class UserRegisterPage extends HookConsumerWidget {
   const UserRegisterPage({
@@ -24,8 +21,10 @@ class UserRegisterPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final auth = FirebaseAuth.instance;
     final email = useState('');
-    final name = useState('');
+    final password = useState('');
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(46.sp),
@@ -33,45 +32,53 @@ class UserRegisterPage extends HookConsumerWidget {
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              onChanged: (value) {
-                email.value = value;
-              },
-              decoration: const InputDecoration(
-                hintText: 'ニックネーム',
-              ),
-            ),
+          SharedTextField(
+            hintText: 'メールアドレスを入力',
+            onChanged: (value) {
+              email.value = value;
+            },
           ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              onChanged: (value) {
-                name.value = value;
-              },
-              decoration: const InputDecoration(
-                hintText: 'メールアドレスを入力',
-              ),
-            ),
+          SharedTextField(
+            hintText: 'パスワードを入力',
+            obscureText: true,
+            onChanged: (value) {
+              password.value = value;
+            },
           ),
           ElevatedButton(
             style: ElevatedButton.styleFrom(backgroundColor: primary),
-            onPressed: () {
-              ref.read(registerUserProvider).registerUser(
-                    user: User(
-                      userName: name.value,
-                      email: email.value,
-                      //TODO: 任意のものに変更できるようにする。
-                      userImagePath:
-                          'assets/images/kkrn_user_icons/kkrn_icon_user_1.png',
-                      accountLevel: AccountLevel.generalUser,
-                      firstTimeUsing: DateTime.now(),
-                    ),
-                  );
-              NavigatorPushReplacement(context,
-                  page: const SakuraSimulationApp());
+            onPressed: () async {
+              try {
+                await auth.createUserWithEmailAndPassword(
+                  email: email.value,
+                  password: password.value,
+                );
+                NavigatorPushReplacement(context, page: SakuraSimulationApp());
+              } on FirebaseAuthException catch (error) {
+                final String errorMessage = handleFirebaseAuthError(error);
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text(errorMessage)),
+                );
+              }
             },
+            // onPressed: () async {
+            //   // ref.read(registerUserProvider).registerUser(
+            //   await _auth.createUserWithEmailAndPassword(
+            //     email: email.value,
+            //     password: password.value,
+            //     //  User(
+            //     //   userName: name.value,
+            //     //   email: email.value,
+            //     //   //TODO: 任意のものに変更できるようにする。
+            //     //   userImagePath:
+            //     //       'assets/images/kkrn_user_icons/kkrn_icon_user_1.png',
+            //     //   accountLevel: AccountLevel.generalUser,
+            //     //   firstTimeUsing: DateTime.now(),
+            //     // ),
+            //   );
+            //   NavigatorPushReplacement(context,
+            //       page: const SakuraSimulationApp());
+            // },
             child: const Text('登録'),
           )
         ],
